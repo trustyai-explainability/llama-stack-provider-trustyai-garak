5b626e834d3dfc0ebb9a421bfcea14e863d39898
saichandrapandraju <saichandrapandraju@gmail.com>
2025-08-20 09:37:38 -0400

    🚀 Add remote eval provider support

--- src/llama_stack_provider_trustyai_garak/provider.py
+++ src/llama_stack_provider_trustyai_garak/provider.py
@@ -1,22 +1,41 @@
 from llama_stack.providers.datatypes import (
     ProviderSpec,
     Api,
+    AdapterSpec,
+    remote_provider_spec,
     InlineProviderSpec
 )
 from llama_stack.apis import (
     inference, 
     files, 
     safety,
-    telemetry
+    telemetry,
+    shields,
+    benchmarks
     )
+from typing import List
 
-def get_provider_spec() -> ProviderSpec:
-    return InlineProviderSpec(
-        api=Api.eval,
-        provider_type="inline::trustyai_garak",
-        pip_packages=["garak"],
-        config_class="config.GarakEvalProviderConfig",
-        module="llama_stack_provider_trustyai_garak",
-        api_dependencies=[inference, files],
-        optional_api_dependencies=[safety, telemetry]
-    )
+
+def get_provider_spec() -> List[ProviderSpec]:
+    return [
+        remote_provider_spec(
+            api=Api.eval,
+            adapter=AdapterSpec(
+                adapter_type="trustyai_garak",
+                module="llama_stack_provider_trustyai_garak",
+                pip_packages=["garak", "kfp", "kfp-kubernetes", "kfp-server-api", "boto3"],
+                config_class="config.GarakRemoteConfig",
+            ),
+            api_dependencies=[inference, files, benchmarks],
+            optional_api_dependencies=[safety, telemetry, shields]
+        ),
+        InlineProviderSpec(
+            api=Api.eval,
+            provider_type="inline::trustyai_garak",
+            pip_packages=["garak"],
+            config_class="config.GarakEvalProviderConfig",
+            module="llama_stack_provider_trustyai_garak",
+            api_dependencies=[inference, files, benchmarks],
+            optional_api_dependencies=[safety, telemetry, shields]
+        )
+    ]
