e84406d1da5e337d7ae7edf230bdcc90918f1c59
saichandrapandraju <saichandrapandraju@gmail.com>
2025-10-06 21:33:05 -0400

    Chore: Use RemoteProviderSpec + Bump lls to 0.2.23

--- src/llama_stack_provider_trustyai_garak/remote/provider.py
+++ src/llama_stack_provider_trustyai_garak/remote/provider.py
@@ -1,18 +1,17 @@
 from llama_stack.providers.datatypes import (
     ProviderSpec,
     Api,
-    AdapterSpec,
-    remote_provider_spec
+    RemoteProviderSpec
 )
 
 def get_provider_spec() -> ProviderSpec:
-    return remote_provider_spec(
+    return RemoteProviderSpec(
             api=Api.eval,
-            adapter=AdapterSpec(
-                adapter_type="trustyai_garak",
-                module="llama_stack_provider_trustyai_garak.remote",
-                pip_packages=["garak", "kfp", "kfp-kubernetes", "kfp-server-api", "boto3"],
-                config_class="llama_stack_provider_trustyai_garak.config.GarakRemoteConfig",
-            ),
-            api_dependencies=[Api.inference, Api.files, Api.benchmarks, Api.safety, Api.telemetry, Api.shields]
+            provider_type="remote::trustyai_garak",
+            config_class="llama_stack_provider_trustyai_garak.config.GarakRemoteConfig",
+            api_dependencies=[Api.inference, Api.files, Api.benchmarks, Api.safety, Api.telemetry, Api.shields],
+            module="llama_stack_provider_trustyai_garak.remote",
+            pip_packages=["garak", "kfp", "kfp-kubernetes", "kfp-server-api", "boto3"],
+            adapter_type="trustyai_garak",
+            description="TrustyAI’s remote provider for Garak vulnerability scanning on Kubeflow Pipelines.",   
         )
